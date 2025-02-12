d64cbcf06d0cd40e1d3dc383d6ce160e18b7afbf
Sameer  Moyal <sameerab827@gmail.com>
2025-02-12 14:24:45 +0530

    update

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose"
 export const connectDB=async()=>{
     try{
     
-        const conn =await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          })
+        const conn =await mongoose.connect(process.env.MONGO_URL)
         console.log("mongodb connected")
     }catch(error){
         console.log("get error to mongodb connection",error)
